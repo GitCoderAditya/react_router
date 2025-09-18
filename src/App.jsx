b17f5e39c7e2263b8aae3861832c824faf7f9704
ashu <majumderaditya03@gmail.com>
2025-09-18 22:40:36 +0530

    intialized new react project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import './App.css'
+
+function App() {
+  
+
+  return (
+
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+     </div>
+  )
+}
+
+export default App
