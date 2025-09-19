726bb9845fdb476e869f44d388c57eeea91a1358
ashu <majumderaditya03@gmail.com>
2025-09-20 00:45:58 +0530

    useref understanding

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,37 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import './App.css'
+import { useRef, useState } from 'react';
 
 function App() {
   
 
+  const [counter, setCounter] = useState(0);
+  const timer = useRef();
+
+  function clockStart() {
+    let value = setInterval(function(){
+      setCounter((c) => c + 1);
+    },1000);
+
+    timer.current = value;
+
+  }
+    function clockStop() {
+      clearInterval(timer.current);
+  }
   return (
 
+
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </BrowserRouter>
+      {counter}
+      <br />
+      <button onClick={clockStart}>Start</button>
+      <button onClick={clockStop}>Stop</button>
      </div>
   )
 }
+
+
+
 
 export default App
